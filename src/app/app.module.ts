0010d40fcf5830bdf97d0022ddbd3f2cd4f0aec3
GrzegorzHomoncik <226397@student.pwr.edu.pl>
2019-11-19 23:08:36 +0100

    File upload + chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FileUploaderComponent } from './file-uploader.component';
+import { FileUploaderService } from './file-uploader.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploaderComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, 
+    HttpClientModule,
+    ChartsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [FileUploaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
