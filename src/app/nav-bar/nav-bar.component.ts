db38a03430e8f6a21458ee776155f9c0cccaccf1
mzy <mzy@hicron.com>
2020-01-20 20:25:35 +0100

    wyswietlanie wykresow w zaleznosci od ilosci wymiarow

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent implements OnInit {
 
